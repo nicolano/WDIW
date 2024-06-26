eba34c9126f01e57ef1697fc387d5ee953011c6e
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-06-26 14:11:16 +0200

    Refactoring

---
+++ WDIW/View/Screens/ContentScreens/ContentHero/ThumbnailContent.swift
@@ -0,0 +1,18 @@
+//
+//  ThumbnailContent.swift
+//  WDIW
+//
+//  Created by Nicolas von Trott on 26.06.24.
+//
+
+import SwiftUI
+
+struct ThumbnailContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThumbnailContent()
+}
